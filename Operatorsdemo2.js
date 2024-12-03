a3425aaa54e219c15227a8e395ea50348f172fc3
Permeshwary(Poonam) Bora <147245184+poonammirchandani123@users.noreply.github.com>
2024-12-03 19:32:33 +0000

    Add files via upload

---
+++ Operatorsdemo2.js
@@ -0,0 +1,20 @@
+ let x=10, y=20;
+ console.log(x+y);
+ console.log(y-x);
+ console.log(x*y);
+ console.log(y%x);
+ console.log(y/x);
+ console.log(5**2);
+ 
+
+
+ //Operatorsdemo2.js
+//let a = 10;
+//let b = 20;
+//console.log("The sum is: ", a + b);
+
+//console.log("Hello, this is a test!");
+
+//console.log("Hello, World!");
+
+
